6f7c0ecdfe57c7dd0bba97cfc15fb174353de1df
supermn <1781394865@qq.com>
2018-02-27 17:46:49 +0800

    1.二维码与条形码生成解析

--- src/test/action/Test.java
+++ src/test/action/Test.java
@@ -1,10 +1,193 @@
 package test.action;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.GlobalHistogramBinarizer;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		
+		// 这是二维码图片  
+        //BufferedImage bi = ImageIO.read(new FileInputStream(new File("E:" + File.separator + "qr_code.jpg")));  
+  
+        // 这是条形码图片  
+        //BufferedImage bi = ImageIO.read(new FileInputStream(new File("E:" + File.separator + "ean3.jpg")));  
+  
+        /*if (bi != null) {  
+            Map<DecodeHintType, String> hints = new HashMap<>();  
+            hints.put(DecodeHintType.CHARACTER_SET, "utf-8");  
+            LuminanceSource source = new BufferedImageLuminanceSource(bi);  
+            // 这里还可以是   
+            //BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));  
+            BinaryBitmap bitmap = new BinaryBitmap(new GlobalHistogramBinarizer(source));  
+            Result res = new MultiFormatReader().decode(bitmap, hints);  
+            System.out.println(res);  
+        }  */
+		Test.ean_code();
+	}
+	
+	 private static final int BLACK = 0xFF000000;  
+	    private static final int WHITE = 0xFFFFFFFF;  
+	  
+	    private static BufferedImage toBufferedImage(BitMatrix bm) {  
+	        int width = bm.getWidth();  
+	        int height = bm.getHeight();  
+	        BufferedImage image = new BufferedImage(width, height,  
+	                BufferedImage.TYPE_3BYTE_BGR);  
+	        for (int i = 0; i < width; i++) {  
+	            for (int j = 0; j < height; j++) {  
+	                image.setRGB(i, j, bm.get(i, j) ? BLACK : WHITE);  
+	            }  
+	        }  
+	        return image;  
+	    }  
+	  
+	    public static void writeBitMatricToFile(BitMatrix bm, String format,  
+	            File file) throws IOException {  
+	        BufferedImage image = toBufferedImage(bm);  
+	        // 设置logo图标 二维码中间logo设置           
+	        // 注意条形码一般中间是不会有图片的、若生成条形码 、下面2行需注释掉  
+	         image = Test.LogoMatrix(image);  
+	        try {  
+	            if (!ImageIO.write(image, format, file)) {  
+	                throw new RuntimeException("Can not write an image to file"  
+	                        + file);  
+	            }  
+	        } catch (IOException e) {  
+	            e.printStackTrace();  
+	        }  
+	    }  
+	  
+	    public static void writeToStream(BitMatrix matrix, String format,  
+	            OutputStream stream) throws IOException {  
+	        BufferedImage image = toBufferedImage(matrix);  
+	        // 设置logo图标 二维码中间logo设置  
+	        // 注意条形码一般中间是不会有图片的、若生成条形码 、下面2行需注释掉  
+	        // LogoConfig logoConfig = new LogoConfig();  
+	        // image = logoConfig.LogoMatrix(image);  
+	        if (!ImageIO.write(image, format, stream)) {  
+	            throw new IOException("Could not write an image of format "  
+	                    + format);  
+	        }  
+	    } 
+	    
+	    /** 
+	     * 设置 logo 
+	     *  
+	     * @param matrixImage 源二维码图片 
+	     * @return 返回带有logo的二维码图片 
+	     * @throws IOException 
+	     * @author Administrator sangwenhao 
+	     */  
+	    public static BufferedImage LogoMatrix(BufferedImage matrixImage)  
+	            throws IOException {  
+	        /** 
+	         * 读取二维码图片，并构建绘图对象 
+	         */  
+	        Graphics2D g2 = matrixImage.createGraphics();  
+	  
+	        int matrixWidth = matrixImage.getWidth();  
+	        int matrixHeigh = matrixImage.getHeight();  
+	  
+	        /** 
+	         * 读取Logo图片 
+	         */  
+	        BufferedImage logo = ImageIO.read(new File("E:\\luvl.jpg"));  
+	  
+	        // 开始绘制图片  
+	        g2.drawImage(logo, matrixWidth / 5 * 2, matrixHeigh / 5 * 2,  
+	                matrixWidth / 5, matrixHeigh / 5, null);// 绘制  
+	        BasicStroke stroke = new BasicStroke(5, BasicStroke.CAP_ROUND,  
+	                BasicStroke.JOIN_ROUND);  
+	        g2.setStroke(stroke);// 设置笔画对象  
+	        // 指定弧度的圆角矩形  
+	        RoundRectangle2D.Float round = new RoundRectangle2D.Float(  
+	                matrixWidth / 5 * 2, matrixHeigh / 5 * 2, matrixWidth / 5,  
+	                matrixHeigh / 5, 20, 20);  
+	        g2.setColor(Color.white);  
+	        g2.draw(round);// 绘制圆弧矩形  
+	  
+	        // 设置logo 有一道灰色边框  
+	        BasicStroke stroke2 = new BasicStroke(1, BasicStroke.CAP_ROUND,  
+	                BasicStroke.JOIN_ROUND);  
+	        g2.setStroke(stroke2);// 设置笔画对象  
+	        RoundRectangle2D.Float round2 = new RoundRectangle2D.Float(  
+	                matrixWidth / 5 * 2 + 2, matrixHeigh / 5 * 2 + 2,  
+	                matrixWidth / 5 - 4, matrixHeigh / 5 - 4, 20, 20);  
+	        g2.setColor(new Color(128, 128, 128));  
+	        g2.draw(round2);// 绘制圆弧矩形  
+	  
+	        g2.dispose();  
+	        matrixImage.flush();  
+	        return matrixImage;  
+	    }  
+	
+	/**
+	 *二维码
+	 * @throws IOException 
+	*/
+	public static void qr_code() throws WriterException, IOException {
+		int width = 300;
+		int height = 300;
+		String text = "https://www.baidu.com/";
+		String format = "png";
+		HashMap<EncodeHintType, Object> hints = new HashMap<>();
+		// 指定纠错等级,纠错级别（L 7%、M 15%、Q 25%、H 30%）
+		hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+		// 内容所使用字符集编码
+		hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+		// 二维码的格式是BarcodeFormat.QR_CODE
+		BitMatrix bm = new MultiFormatWriter().encode(text,
+				BarcodeFormat.QR_CODE, width, height, hints);
+		// 生成二维码图片
+		// File out = new File("qr_code.png"); //默认项目根目录里
+		File out = new File("E:" + File.separator + "qr_code.png"); // 指定输出路径  File.separator解决跨平台
+		Test.writeBitMatricToFile(bm, format, out);
 	}
 
+	/**
+	 *条形码
+	 * @throws IOException 
+	*/
+	public static void ean_code() throws WriterException, IOException {
+		int width = 200;
+		int height = 100;
+		String text = "6923450657713";
+		String format = "png";
+		HashMap<EncodeHintType, String> hints = new HashMap<>();
+		hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+		// 条形码的格式是 BarcodeFormat.EAN_13
+		BitMatrix bm = new MultiFormatWriter().encode(text,
+				BarcodeFormat.EAN_13, width, height, hints);
+		// 生成条形码图片
+		// File out = new File("ean3.png");
+		File out = new File("E:" + File.separator + "ean3.png");// 指定输出路径 // File.separator解决跨平台
+		Test.writeBitMatricToFile(bm, format, out);
+	}
+	
 }
--- src/test/action/Test1.java
+++ src/test/action/Test1.java
@@ -1,17 +1,268 @@
 package test.action;
 
+import com.google.zxing.*;  
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;  
+import com.google.zxing.common.BitMatrix;  
+import com.google.zxing.common.HybridBinarizer;  
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;  
+  
+import javax.imageio.ImageIO;  
+import java.awt.*;  
+import java.awt.geom.RoundRectangle2D;  
+import java.awt.image.BufferedImage;  
+import java.io.File;  
+import java.io.OutputStream;  
+import java.util.Hashtable;  
+import java.util.Random;  
+
 public class Test1 {
 	
-	public void bbb() {
-	}
-	
-	public static void main(String[] args) {
-		return;
-
-	}
-	
-	public void aaa() {
-		return;
-	}
+		// 字符集
+	 	private static final String CHARSET = "utf-8"; 
+	 	// 图片格式 
+	    private static final String FORMAT_NAME = "jpg";  
+	    // 二维码尺寸  
+	    private static final int QRCODE_SIZE = 300;  
+	    // LOGO宽度  
+	    private static final int WIDTH = 60;  
+	    // LOGO高度  
+	    private static final int HEIGHT = 60;  
+	  
+	    /**  
+	     * 生成二维码的方法  
+	     *  
+	     * @param content      目标URL  
+	     * @param imgPath      LOGO图片地址  
+	     * @param needCompress 是否压缩LOGO  
+	     * @return 二维码图片  
+	     * @throws Exception  
+	     */  
+	    private static BufferedImage createImage(String content, String imgPath,  
+	                                             boolean needCompress) throws Exception {  
+	    	//设置二维码纠错级别ＭＡＰ
+	        Hashtable<EncodeHintType, Object> hints = new Hashtable<EncodeHintType, Object>();  
+	        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);  // 矫错级别  
+	        hints.put(EncodeHintType.CHARACTER_SET, CHARSET);  
+	        hints.put(EncodeHintType.MARGIN, 1);  
+	        //创建比特矩阵(位矩阵)的QR码编码的字符串  
+	        BitMatrix bitMatrix = new MultiFormatWriter().encode(content,  
+	                BarcodeFormat.QR_CODE, QRCODE_SIZE, QRCODE_SIZE, hints);  
+	        // 使BufferedImage勾画QRCode 
+	        int width = bitMatrix.getWidth();  
+	        int height = bitMatrix.getHeight();  
+	        BufferedImage image = new BufferedImage(width, height,  
+	                BufferedImage.TYPE_INT_RGB);  
+	        // 使用比特矩阵画并保存图像
+	        for (int x = 0; x < width; x++) {  
+	            for (int y = 0; y < height; y++) {  
+	                image.setRGB(x, y, bitMatrix.get(x, y) ? 0xFF000000  
+	                        : 0xFFFFFFFF);  
+	            }  
+	        }  
+	        if (imgPath == null || "".equals(imgPath)) {  
+	            return image;  
+	        }  
+	        // 插入图片  
+	        Test1.insertImage(image, imgPath, needCompress);  
+	        return image;  
+	    }  
+	  
+	  
+	    /**  
+	     * 插入LOGO  
+	     *  
+	     * @param source       二维码图片  
+	     * @param imgPath      LOGO图片地址  
+	     * @param needCompress 是否压缩  
+	     * @throws Exception  
+	     */  
+	    private static void insertImage(BufferedImage source, String imgPath,  
+	                                    boolean needCompress) throws Exception {  
+	        File file = new File(imgPath);  
+	        if (!file.exists()) {  
+	            System.err.println("" + imgPath + "   该文件不存在！");  
+	            return;  
+	        }  
+	        Image src = ImageIO.read(new File(imgPath));  
+	        int width = src.getWidth(null);  
+	        int height = src.getHeight(null);  
+	        if (needCompress) { // 压缩LOGO  
+	            if (width > WIDTH) {  
+	                width = WIDTH;  
+	            }  
+	            if (height > HEIGHT) {  
+	                height = HEIGHT;  
+	            }  
+	            Image image = src.getScaledInstance(width, height,  
+	                    Image.SCALE_SMOOTH);  
+	            BufferedImage tag = new BufferedImage(width, height,  
+	                    BufferedImage.TYPE_INT_RGB);  
+	            Graphics g = tag.getGraphics();  
+	            g.drawImage(image, 0, 0, null); // 绘制缩小后的图  
+	            g.dispose();  
+	            src = image;  
+	        }  
+	        // 插入LOGO  
+	        Graphics2D graph = source.createGraphics();  
+	        int x = (QRCODE_SIZE - width) / 2;  
+	        int y = (QRCODE_SIZE - height) / 2;  
+	        graph.drawImage(src, x, y, width, height, null);  
+	        Shape shape = new RoundRectangle2D.Float(x, y, width, width, 6, 6);  
+	        graph.setStroke(new BasicStroke(3f));  
+	        graph.draw(shape);  
+	        graph.dispose();  
+	    }  
+	  
+	    /**  
+	     * 生成二维码(内嵌LOGO)  
+	     *  
+	     * @param content      内容  
+	     * @param imgPath      LOGO地址  
+	     * @param destPath     存放目录  
+	     * @param needCompress 是否压缩LOGO  
+	     * @throws Exception  
+	     */  
+	    public static void encode(String content, String imgPath, String destPath,  
+	                              boolean needCompress) throws Exception {  
+	        BufferedImage image = Test1.createImage(content, imgPath,  
+	                needCompress);  
+	        mkdirs(destPath);  
+	        String file = new Random().nextInt(99999999) + "." + FORMAT_NAME;  
+	        ImageIO.write(image, FORMAT_NAME, new File(destPath + "/" + file));  
+	    }  
+	  
+	    /**  
+	     * 当文件夹不存在时，mkdirs会自动创建多层目录，区别于mkdir．(mkdir如果父目录不存在则会抛出异常)  
+	     *  
+	     * @param destPath 存放目录  
+	     */  
+	    public static void mkdirs(String destPath) {  
+	        File file = new File(destPath);  
+	        if (!file.exists() && !file.isDirectory()) {  
+	            file.mkdirs();  
+	        }  
+	    }  
+	  
+	    /**  
+	     * 生成二维码(内嵌LOGO)  
+	     *  
+	     * @param content  内容  
+	     * @param imgPath  LOGO地址  
+	     * @param destPath 存储地址  
+	     * @throws Exception  
+	     */  
+	    public static void encode(String content, String imgPath, String destPath)  
+	            throws Exception {  
+	        Test1.encode(content, imgPath, destPath, false);  
+	    }  
+	  
+	    /**  
+	     * 生成二维码  
+	     *  
+	     * @param content      内容  
+	     * @param destPath     存储地址  
+	     * @param needCompress 是否压缩LOGO  
+	     * @throws Exception  
+	     */  
+	    public static void encode(String content, String destPath,  
+	                              boolean needCompress) throws Exception {  
+	        Test1.encode(content, null, destPath, needCompress);  
+	    }  
+	  
+	    /**  
+	     * 生成二维码  
+	     *  
+	     * @param content  内容  
+	     * @param destPath 存储地址  
+	     * @throws Exception  
+	     */  
+	    public static void encode(String content, String destPath) throws Exception {  
+	        Test1.encode(content, null, destPath, false);  
+	    }  
+	  
+	    /**  
+	     * 生成二维码(内嵌LOGO)  
+	     *  
+	     * @param content      内容  
+	     * @param imgPath      LOGO地址  
+	     * @param output       输出流  
+	     * @param needCompress 是否压缩LOGO  
+	     * @throws Exception  
+	     */  
+	    public static void encode(String content, String imgPath,  
+	                              OutputStream output, boolean needCompress) throws Exception {  
+	        BufferedImage image = Test1.createImage(content, imgPath,  
+	                needCompress);  
+	        ImageIO.write(image, FORMAT_NAME, output);  
+	    }  
+	  
+	    /**  
+	     * 生成二维码  
+	     *  
+	     * @param content 内容  
+	     * @param output  输出流  
+	     * @throws Exception  
+	     */  
+	    public static void encode(String content, OutputStream output)  
+	            throws Exception {  
+	        Test1.encode(content, null, output, false);  
+	    }  
+	  
+	    /**  
+	     * 解析二维码  
+	     *  
+	     * @param file 二维码图片  
+	     * @return  
+	     * @throws Exception  
+	     */  
+	    public static String decode(File file) throws Exception {  
+	        BufferedImage image;  
+	        image = ImageIO.read(file);  
+	        if (image == null) {  
+	            return null;  
+	        }  
+	        BufferedImageLuminanceSource source = new BufferedImageLuminanceSource(  
+	                image);  
+	        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));  
+	        Result result;  
+	        Hashtable<DecodeHintType, Object> hints = new Hashtable<DecodeHintType, Object>();  
+	        hints.put(DecodeHintType.CHARACTER_SET, CHARSET);  
+	        result = new MultiFormatReader().decode(bitmap, hints);  
+	        String resultStr = result.getText();  
+	        return resultStr;  
+	    }  
+	  
+	    /**  
+	     * 解析二维码  
+	     *  
+	     * @param path 二维码图片地址  
+	     * @return  不是二维码的内容返回null,是二维码直接返回识别的结果  
+	     * @throws Exception  
+	     */  
+	    public static String decode(String path) throws Exception {  
+	        return Test1.decode(new File(path));  
+	    }  
+	  
+	    public static void main(String[] args)  {  
+	  
+	        // 生成二维码  
+	        String text = "1234a567890";  
+	        String imagePath = System.getProperty("user.dir") + "/data/1.jpg";  
+	        String destPath = System.getProperty("user.dir") + "/data/output/";  
+	        try {
+	        	Test1.encode(text, imagePath, destPath, true);  
+	        }catch (Exception e){  
+	            e.printStackTrace();  
+	        }
+	        //验证图片是否含有二维码  
+	        /*String destPath1 = System.getProperty("user.dir") + "/data/3.jpg";  
+	        try {  
+	            String result = decode(destPath1);  
+	            System.out.println(result);  
+	        }catch (Exception e){  
+	            e.printStackTrace();  
+	            System.out.println(destPath1+"不是二维码");  
+	        }  */
+	    }  
 
 }
